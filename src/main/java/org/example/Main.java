f79d7d3c2985d5c0aac8a29eae63c457ab0b1523
Filip Kristijan Rankovic Singidunum <filip.kristijan.rankovic.22@singimail.rs>
2024-05-15 15:29:11 +0200

    added token to token.txt

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,17 @@
 package org.example;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) {
         try {
-            List<Question> questions = QuestionService.getInstance()
-                    .getQuestions("");
+            String token = Files.readString(Paths.get("token.txt"), StandardCharsets.UTF_8).replaceAll("\n", "");
+            List<Question> questions = QuestionService.getInstance().getQuestions(token);
             questions.stream().map(Question::getText).forEach(System.out::println);
         } catch (IOException e) {
             throw new RuntimeException(e);
