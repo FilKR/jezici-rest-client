03cc8433b27144c4f41f40759a43042972901a71
Filip Kristijan Rankovic Singidunum <filip.kristijan.rankovic.22@singimail.rs>
2024-05-15 15:22:03 +0200

    added token.txt

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         try {
             List<Question> questions = QuestionService.getInstance()
-                    .getQuestions("3076dbc4-aca5-4073-9e58-1e72587f4170");
+                    .getQuestions("");
             questions.stream().map(Question::getText).forEach(System.out::println);
         } catch (IOException e) {
             throw new RuntimeException(e);
